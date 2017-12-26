10b8bd4eddcd99cd4cf00ed1cec9a79166c383d8
KhalidElKhamlichi <khalid.elboukhari.k@gmail.com>
2017-12-26 12:54:38 +0000

    Remove unnecessary functionality

--- src/app/services/upload-file.service.ts
+++ src/app/services/upload-file.service.ts
@@ -12,13 +12,16 @@ export class UploadFileService {
  
   pushFileToStorage(file: File, userId: string, progress: {percentage: number}, onUpload: (success) => void) {
 
+    // get a reference to firebase storage
     const storageRef = firebase.storage().ref();
+    // upload the file to the specified path
     const uploadTask = storageRef.child(`${this.basePath}/${userId}/${file.name}`).put(file, { contentType: file.type});
  
     uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED,
       (snapshot) => {
         // in progress
         const snap = snapshot as firebase.storage.UploadTaskSnapshot
+        // update progress bar
         progress.percentage += Math.round((snap.bytesTransferred / snap.totalBytes) * 100)
       },
       (error) => {
@@ -28,14 +31,9 @@ export class UploadFileService {
       },
       () => {
         // success
-        this.saveFileData(file);
         onUpload(true);
       }
     );
     
-  }
- 
-  private saveFileData(file: File) {
-    this.db.list(`${this.basePath}/`).push(file);
   }
 }
